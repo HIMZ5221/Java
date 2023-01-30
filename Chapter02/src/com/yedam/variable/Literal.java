c32da1e8330de95a5ed8f29367c4d855b8198c37
HIMZ5221 <123717096+HIMZ5221@users.noreply.github.com>
2023-01-30 17:42:03 +0900

    Add files via upload

---
+++ Chapter02/src/com/yedam/variable/Literal.java
@@ -0,0 +1,20 @@
+package com.yedam.variable;
+
+public class Literal {
+	public static void main(String[] args) {
+		
+		
+		//int 사용
+		int var1 = 0b1011; //2진수  0b로 시작
+		int var2 = 0206; //8진수 0으로 시작
+		int var3 = 365; //10진수
+		int var4 = 0xB3; //16진수 0x로 시작
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		
+	}
+}
+
